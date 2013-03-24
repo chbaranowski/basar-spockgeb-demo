621279fd73b8d6abc610714fe9a4aea7c8632b01
Christian Baranowski <tux2323@pro-pix.de>
2013-03-24 18:39:43 +0100

    Code Style changed.

--- src/test/groovy/basar/BasarSpec.groovy
+++ src/test/groovy/basar/BasarSpec.groovy
@@ -19,42 +19,40 @@ import data.UserRepository;
 import static org.mockito.Mockito.*;
 
 class BasarSpec extends BasarWebSpec {
-	
-	User testUser
-	
-	@Autowired
-	UserRepository userRepository
-	
-	@Override
-	def enviorment() { "production" }
-	
-	def setup() {
-		testUser = userRepository.save(new User(basarNumber: "100"))
-	}
-	
-	def cleanup() {
-		userRepository.delete(testUser.getId())
-		testUser = null
-	}
-	
-	@Unroll
-	def "buy some articles with number='#numberValue' and price='#priceValue'"() {
-		given:
-			to BasarPage
-		when: "add a article into the cart"
-			basarForm.with {
-				basarNumber = numberValue
-				price = priceValue
-			}
-			addButton.click()
-		then: "the article should be in the cart"
-			basarNumber(cartItem: 0) == numberValue
-			price(cartItem: 0) == sumValue
-			sum == sumValue
-		where:
-			numberValue   | priceValue   ||   sumValue
-			"100"         |   "10"       ||   "10,00"
-			"100"         |   "10,50"    ||   "10,50"	
-	}
 
+    User testUser
+
+    @Autowired
+    UserRepository userRepository
+
+    @Override
+    def enviorment() { "production" }
+
+    def setup() {
+        testUser = userRepository.save(new User(basarNumber: "100"))
+    }
+
+    def cleanup() {
+        userRepository.delete(testUser.getId())
+    }
+
+    @Unroll
+    def "buy some articles with number='#numberValue' and price='#priceValue'"(String numberValue, String priceValue, String sumValue) {
+        given:
+            to BasarPage
+        when: "add a article into the cart"
+            basarForm.with {
+                basarNumber = numberValue
+                price = priceValue
+            }
+            addButton.click()
+        then: "the article should be in the cart"
+            basarNumber(cartItem: 0)    == numberValue
+            price(cartItem: 0)          == sumValue
+            sum == sumValue
+        where:
+            numberValue   | priceValue   ||   sumValue
+            "100"         |   "10"       ||   "10,00"
+            "100"         |   "10,50"    ||   "10,50"
+    }
 }
