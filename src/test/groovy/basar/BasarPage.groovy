44faaac2b935e344e81e74a60fe653765970d9e4
Christian Baranowski <tux2323@pro-pix.de>
2013-03-24 19:39:08 +0100

    reformat the code style

--- src/test/groovy/basar/BasarPage.groovy
+++ src/test/groovy/basar/BasarPage.groovy
@@ -3,29 +3,27 @@ package basar
 import geb.Page;
 
 class BasarPage extends Page {
-	
-	static url = "/static/basar.html"
-	
-	static at = { title == "Basar" }
-	
-	static content = {
-		
-		basarForm { $("form") }
-		
-		addButton { $("#addCartItem") }
-		
-		sum { $("#sum").text() }
-		
-		basarNumber(wait:true) { attr ->
-			 def index = attr.cartItem * 4
-			 $("td")[index].text()
-		}
-		
-		price(wait:true) { attr ->
-			def index = (attr.cartItem * 4) + 1
-			$("td")[index].text()
-	   }
-		
-	}
 
+    static url = "/static/basar.html"
+
+    static at = { title == "Basar" }
+
+    static content = {
+
+        basarForm { $("form") }
+
+        addButton { $("#addCartItem") }
+
+        sum { $("#sum").text() }
+
+        basarNumber(wait:true) { attr ->
+            def index = attr.cartItem * 4
+            $("td")[index].text()
+        }
+
+        price(wait:true) { attr ->
+            def index = (attr.cartItem * 4) + 1
+            $("td")[index].text()
+        }
+    }
 }
