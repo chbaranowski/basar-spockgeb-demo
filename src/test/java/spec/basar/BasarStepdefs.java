75b30a79abfa4e110e06bf141a43d045c946d7b9
Christian Baranowski <tux2323@pro-pix.de>
2013-05-01 17:36:43 +0200

    Removed unused imports

--- src/test/java/spec/basar/BasarStepdefs.java
+++ src/test/java/spec/basar/BasarStepdefs.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
