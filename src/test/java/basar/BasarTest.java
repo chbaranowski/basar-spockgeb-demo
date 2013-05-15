04b3eb2683078fdb5bf7331357ccde41a28652ee
Christian Baranowski <baranowski@N-Miraculix.seitenbau.net>
2013-05-15 15:37:05 +0200

    Fluent Wait

--- src/test/java/basar/BasarTest.java
+++ src/test/java/basar/BasarTest.java
@@ -2,16 +2,24 @@ package basar;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import com.google.common.base.Function;
 
 import controller.UserResource;
 
@@ -22,6 +30,8 @@ public class BasarTest {
     static Webapp webapp;
     
     WebDriver driver;
+    
+    Wait<WebDriver> wait;
 
     @BeforeClass
     public static void startApp() throws Exception {
@@ -52,11 +62,17 @@ public class BasarTest {
         driver.findElement(By.id("lastname")).sendKeys("Tester");
         driver.findElement(By.id("email")).sendKeys("Test@Test,de");
         driver.findElement(By.id("saveUser")).click();
-        for (int second = 0;; second++) {
-            if (second >= 60) fail("timeout");
-            try { if ("100".equals(driver.findElement(By.cssSelector("td")).getText())) break; } catch (Exception e) {}
-            Thread.sleep(1000);
-        }
+        
+        wait = new FluentWait<WebDriver>(driver)
+                    .withTimeout(30, TimeUnit.SECONDS)
+                    .pollingEvery(200, TimeUnit.MILLISECONDS)
+                    .ignoring(NoSuchElementException.class);
+        
+        wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.cssSelector("td"));
+            }
+        });
     }
     
     @After
@@ -72,11 +88,17 @@ public class BasarTest {
         driver.findElement(By.id("price")).clear();
         driver.findElement(By.id("price")).sendKeys("10,50");
         driver.findElement(By.id("addCartItem")).click();
-        for (int second = 0;; second++) {
-            if (second >= 60) fail("timeout");
-            try { if ("10,50".equals(driver.findElement(By.id("sum")).getText())) break; } catch (Exception e) {}
-            Thread.sleep(1000);
-        }
-        assertEquals("10,50", driver.findElement(By.id("sum")).getText());
+        
+        WebElement sum = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                WebElement sum = driver.findElement(By.id("sum"));
+                if(sum.getText().equals("10,50")){
+                    return sum;
+                } else {
+                    return null;
+                }
+            }
+        });
+        assertEquals("10,50", sum.getText());
     }
 }
